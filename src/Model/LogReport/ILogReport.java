84aae977349034993ad847d4eb07f5264959616b
filippolobue <filippo.lobue1993@gmail.com>
2015-10-21 23:18:19 +0200

    cambio progetto...

--- src/Model/LogReport/ILogReport.java
+++
@@ -1,9 +0,0 @@
-package Model.LogReport;
-
-import java.io.IOException;
-
-public interface ILogReport {
-
-	public void report(String who, String message) throws IOException;
-	public void close() throws IOException;
-}
